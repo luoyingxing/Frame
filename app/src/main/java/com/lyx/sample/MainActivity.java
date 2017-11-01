a09389a72a9eec9c9bae19378f01ff169e3457a3
luoyingxing <luoyingxing@126.com>
2017-11-01 17:27:06 +0800

    首页增加了RecyclerView的演示

--- app/src/main/java/com/lyx/sample/MainActivity.java
+++ app/src/main/java/com/lyx/sample/MainActivity.java
@@ -1,17 +1,33 @@
 package com.lyx.sample;
 
+import android.graphics.Rect;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.lyx.frame.annotation.Id;
 import com.lyx.frame.annotation.IdParser;
 import com.lyx.frame.slide.SlideView;
+import com.lyx.sample.entity.Image;
 import com.lyx.sample.entity.SlideInfo;
+import com.lyx.sample.frame.recycler.ViewHolder;
+import com.lyx.sample.frame.recycler.XAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     @Id(R.id.sv_home)
     private SlideView<SlideInfo> mSlideView;
+    @Id(R.id.rv_home)
+    private RecyclerView mRecyclerView;
+
+    private XAdapter<Image> mXAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +42,61 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, info.getTitle(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        mXAdapter = new XAdapter<Image>(MainActivity.this, new ArrayList<Image>(), R.layout.item_image_list) {
+            @Override
+            public void convert(ViewHolder holder, Image item) {
+                holder.setText(R.id.tv_image_title, "相册 " + item.getTitle());
+
+                SimpleDraweeView imageView = holder.getView(R.id.iv_image_image);
+                imageView.setImageURI(Uri.parse(item.getUrl()));
+            }
+        };
+
+        mRecyclerView.setAdapter(mXAdapter);
+        mRecyclerView.addItemDecoration(new SpaceItemDecoration(2));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        mXAdapter.setOnItemClickListener(new XAdapter.OnItemClickListeners<Image>() {
+            @Override
+            public void onItemClick(ViewHolder holder, Image item, int position) {
+                Toast.makeText(MainActivity.this, "第" + position + "张", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(ViewHolder holder, Image item, int position) {
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mXAdapter.addAll(Image.getImageList());
+    }
+
+    private class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+        int mSpace;
+
+        public SpaceItemDecoration(int space) {
+            this.mSpace = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int itemCount = mXAdapter.getItemCount();
+            int pos = parent.getChildAdapterPosition(view);
+
+            outRect.left = mSpace;
+            outRect.top = mSpace;
+            outRect.bottom = mSpace;
+
+            if (pos != (itemCount - 1)) {
+                outRect.right = mSpace;
+            } else {
+                outRect.right = 0;
+            }
+        }
     }
 }
