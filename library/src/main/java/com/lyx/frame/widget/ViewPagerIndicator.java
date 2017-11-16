9fbeca89421957eaa4fa73840dd086c2e9e3f7f4
luoyingxing <luoyingxing@126.com>
2017-11-16 22:39:36 +0800

    增加了选项卡ViewPagerIndicator

---
+++ library/src/main/java/com/lyx/frame/widget/ViewPagerIndicator.java
@@ -0,0 +1,396 @@
+package com.lyx.frame.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.lyx.frame.annotation.TabText;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * ViewPagerIndicator  传入泛型实体时，需要使用{TabText}表明文字字段
+ * <p/>
+ * create by luoyingxing on 2016/10/09
+ */
+public class ViewPagerIndicator<T> extends LinearLayout {
+    /**
+     * 绘制三角形的画笔
+     */
+    private Paint mPaint;
+    /**
+     * path构成一个三角形
+     */
+    private Path mPath;
+    /**
+     * 三角形的宽度
+     */
+    private int mTriangleWidth;
+    /**
+     * 三角形的高度
+     */
+    private int mTriangleHeight;
+
+    /**
+     * 三角形的宽度为单个Tab的1/6
+     */
+    private static final float RADIO_TRIANGLE = 1.0f / 6;
+    /**
+     * 三角形的最大宽度
+     */
+    private final int DIMENSION_TRIANGLE_WIDTH = (int) (getScreenWidth() / 3 * RADIO_TRIANGLE);
+
+    /**
+     * 初始时，三角形指示器的偏移量
+     */
+    private int mInitTranslationX;
+    /**
+     * 手指滑动时的偏移量
+     */
+    private float mTranslationX;
+
+    /**
+     * 默认的Tab数量
+     */
+    private static final int COUNT_DEFAULT_TAB = 0;
+    /**
+     * tab数量
+     */
+    private int mTabVisibleCount = COUNT_DEFAULT_TAB;
+
+    /**
+     * tab上的内容
+     */
+    private List<T> mTabTitles;
+    /**
+     * 与之绑定的ViewPager
+     */
+    public ViewPager mViewPager;
+
+    /**
+     * 标题正常时的颜色
+     */
+    private static final int COLOR_TEXT_NORMAL = 0x77FFFFFF;
+    /**
+     * 标题选中时的颜色
+     */
+    private static final int COLOR_TEXT_HIGH_LIGHT_COLOR = 0xFFFFFFFF;
+
+    public ViewPagerIndicator(Context context) {
+        this(context, null);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // 初始化画笔
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.parseColor("#ffffffff"));
+        mPaint.setStyle(Style.FILL);
+        mPaint.setPathEffect(new CornerPathEffect(3));
+    }
+
+    /**
+     * 绘制指示器
+     */
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        canvas.save();
+        // 画笔平移到正确的位置
+        canvas.translate(mInitTranslationX + mTranslationX, getHeight() + 1);
+        canvas.drawPath(mPath, mPaint);
+        canvas.restore();
+        super.dispatchDraw(canvas);
+    }
+
+    /**
+     * 初始化三角形的宽度
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mTriangleWidth = (int) (w / mTabVisibleCount * RADIO_TRIANGLE);// 1/6 of
+        // width
+        mTriangleWidth = Math.min(DIMENSION_TRIANGLE_WIDTH, mTriangleWidth);
+
+        // 初始化三角形
+        initTriangle();
+
+        // 初始时的偏移量
+        mInitTranslationX = getWidth() / mTabVisibleCount / 2 - mTriangleWidth / 2;
+    }
+
+    /**
+     * 设置可见的tab的数量
+     *
+     * @param count
+     */
+    public void setVisibleTabCount(int count) {
+        this.mTabVisibleCount = count;
+    }
+
+    /**
+     * 设置tab的标题内容 可选，可以自己在布局文件中写死
+     *
+     * @param list
+     */
+    public void setTabItemTitles(List<T> list) {
+        // 如果传入的list有值，则移除布局文件中设置的view
+        if (list != null && list.size() > 0) {
+            this.removeAllViews();
+            this.mTabTitles = list;
+
+            for (T obj : mTabTitles) {
+                addView(generateTextView(getImageUrl(obj)));
+            }
+            // 设置item的click事件
+            setItemClickEvent();
+        }
+
+    }
+
+    private String getImageUrl(T obj) {
+        if (obj instanceof String) {
+            //如果是String类型，直接返回
+            return (String) obj;
+        }
+
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(TabText.class)) {
+                try {
+                    field.setAccessible(true);
+                    return (String) field.get(obj);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 对外的ViewPager的回调接口
+     */
+    public interface PageChangeListener {
+        void onPageScrolled(int position, float positionOffset, int positionOffsetPixels);
+
+        void onPageSelected(int position);
+
+        void onPageScrollStateChanged(int state);
+    }
+
+    // 对外的ViewPager的回调接口
+    private PageChangeListener onPageChangeListener;
+
+    // 对外的ViewPager的回调接口的设置
+    public void setOnPageChangeListener(PageChangeListener pageChangeListener) {
+        this.onPageChangeListener = pageChangeListener;
+    }
+
+    // 设置关联的ViewPager
+    public void setViewPager(ViewPager mViewPager, int pos) {
+        this.mViewPager = mViewPager;
+
+        mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // 设置字体颜色高亮
+                resetTextViewColor();
+                highLightTextView(position);
+
+                // 回调
+                if (onPageChangeListener != null) {
+                    onPageChangeListener.onPageSelected(position);
+                }
+            }
+
+            /**
+             *
+             * @param position 当前下标
+             * @param positionOffset 当前下标为基准的偏移量，取值为 0.00...~9.99...
+             * @param positionOffsetPixels 移动的像素
+             */
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // 滚动
+                scroll(position, positionOffset);
+
+                // 回调
+                if (onPageChangeListener != null) {
+                    onPageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // 回调
+                if (onPageChangeListener != null) {
+                    onPageChangeListener.onPageScrollStateChanged(state);
+                }
+
+            }
+        });
+        // 设置当前页
+        mViewPager.setCurrentItem(pos);
+        // 高亮
+        highLightTextView(pos);
+    }
+
+    /**
+     * 高亮文本
+     *
+     * @param position int
+     */
+    protected void highLightTextView(int position) {
+        View view = getChildAt(position);
+        if (view instanceof TextView) {
+            ((TextView) view).setTextColor(COLOR_TEXT_HIGH_LIGHT_COLOR);
+        }
+    }
+
+    /**
+     * 重置文本颜色
+     */
+    private void resetTextViewColor() {
+        for (int i = 0; i < getChildCount(); i++) {
+            View view = getChildAt(i);
+            if (view instanceof TextView) {
+                ((TextView) view).setTextColor(COLOR_TEXT_NORMAL);
+            }
+        }
+    }
+
+    /**
+     * 设置点击事件
+     */
+    public void setItemClickEvent() {
+        int cCount = getChildCount();
+        for (int i = 0; i < cCount; i++) {
+            final int j = i;
+            View view = getChildAt(i);
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mViewPager.setCurrentItem(j);
+                }
+            });
+        }
+    }
+
+    /**
+     * 根据标题生成我们的TextView
+     *
+     * @param text String
+     * @return TextView
+     */
+    private TextView generateTextView(String text) {
+        TextView tv = new TextView(getContext());
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        lp.width = getScreenWidth() / mTabVisibleCount;
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(COLOR_TEXT_NORMAL);
+        tv.setText(text);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        tv.setLayoutParams(lp);
+        return tv;
+    }
+
+    /**
+     * 初始化三角形指示器
+     */
+    private void initTriangle() {
+        mPath = new Path();
+
+        mTriangleHeight = (int) (mTriangleWidth / 2 / Math.sqrt(2));
+        mPath.moveTo(0, 0);
+        mPath.lineTo(mTriangleWidth, 0);
+        mPath.lineTo(mTriangleWidth / 2, -mTriangleHeight);
+        mPath.close();
+    }
+
+    /**
+     * 指示器跟随手指滚动，以及容器滚动
+     *
+     * @param position 下标
+     * @param offset   下标的偏移量
+     */
+    public void scroll(int position, float offset) {
+        /**
+         * <pre>
+         *  0-1:position=0 ;1-0:postion=0;
+         * </pre>
+         */
+        // 不断改变偏移量，invalidate
+        mTranslationX = getWidth() / mTabVisibleCount * (position + offset);
+
+        int tabWidth = getScreenWidth() / mTabVisibleCount;
+
+        // 容器滚动，当移动到倒数最后一个的时候，开始滚动
+        if (offset > 0 && position >= (mTabVisibleCount - 2)
+                && getChildCount() > mTabVisibleCount) {
+            if (mTabVisibleCount != 1) {
+                this.scrollTo((position - (mTabVisibleCount - 2)) * tabWidth + (int) (tabWidth * offset), 0);
+            } else {
+                // 为count为1时 的特殊处理
+                this.scrollTo(position * tabWidth + (int) (tabWidth * offset), 0);
+            }
+        }
+
+        invalidate();
+    }
+
+    /**
+     * 设置布局中view的一些必要属性；如果设置了setTabTitles，布局中view则无效
+     */
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        int count = getChildCount();
+        if (count == 0) {
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            View view = getChildAt(i);
+            LayoutParams lp = (LayoutParams) view.getLayoutParams();
+            lp.weight = 0;
+            lp.width = getScreenWidth() / mTabVisibleCount;
+            view.setLayoutParams(lp);
+        }
+        // 设置点击事件
+        setItemClickEvent();
+
+    }
+
+    /**
+     * 获得屏幕的宽度
+     *
+     * @return int
+     */
+    public int getScreenWidth() {
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+}
